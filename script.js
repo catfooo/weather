5534b577af30a606a679683f5e881a36c9d91539
So Youn Choi <manghatuhyo@gmail.com>
2023-09-11 17:57:59 +0100

    weather tempature sunrise sunset

--- script.js
+++ script.js
@@ -5,7 +5,19 @@ const fetchWeather = () => {
         console.log(data)
         const weather = data.weather[0].main;
         console.log("Weather:", weather);
-        document.getElementById("weather").innerText = weather;
+        // document.getElementById("weather").innerText = weather;
+        const tempature = Math.round(data.main.temp);
+        console.log("Tempature:", tempature);
+        // document.getElementById("tempature").innerText = tempature;
+        const element = document.getElementById("api");
+        const content = element.innerHTML;
+        element.innerHTML = `${weather} | ${tempature}°`
+        const sunrise = data.sys.sunrise;
+        console.log("sunrise:", sunrise);
+        document.getElementById("sunrise").innerText = sunrise;
+        const sunset = data.sys.sunset;
+        console.log("sunset:", sunset);
+        document.getElementById("sunset").innerText = sunset;
       })
      
       .catch((error) => console.error("Error:", error))
